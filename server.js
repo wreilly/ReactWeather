c85ef4d53b1caa1e3b2bba0d8d38b91b23456221
William Reilly <wreilly2001@gmail.com>
2016-08-06 14:45:00 -0400

    Prep repo for Heroku! HTTP and PORT etc.

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ var express = require('express');
 
 // Create our app
 var app = express();
+// ES6 const. value can't be changed.
+const PORT = process.env.PORT || 3000;
+// hard-coding for local
+// Heroku will pass PORT in ...
+
+// Express middleware -- with every Request:
+app.use(function (req, res, next) {
+	// HTTP = OK
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else  // HTTPS = Not OK. Make it http
+	{
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-	console.log('Express server is up on port 3000. Have Weather fun.');
+
+
+app.listen(PORT, function () {
+	console.log('Express server is up on port ' + PORT + '. Have Weather fun.');
     });
