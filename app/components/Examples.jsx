9ea7ec3e94cfc0889d274e3914788b004db5a05f
William Reilly <wreilly2001@gmail.com>
2016-08-06 14:17:45 -0400

    Added a paragraph, just to see it appearon Github

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
   render : function () {
     return (
+      <div>
         <h3>Some Examples stuff git whoop!</h3>
+        <p>Here's my change for going to Github</p>
+      </div>
     );
   }
 });
