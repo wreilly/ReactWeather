b4e31e8cb5276eb27c326cbde6750e66402dbdc7
William Reilly <wreilly2001@gmail.com>
2016-08-07 12:09:13 -0400

    trying PORT

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 // &&&&&&&&&&&&&&&&&&&&&&&&
 app.use(function (req, res, next) {
 			console.log("WR__ 00 req.hostname + req.url + req.protocol + req.secure : "  + req.hostname + " : " + req.url + " : " + req.protocol + " : " + req.secure);
+			console.log("WR__ 00A PORT STUFF req.hostname + :PORT + req.url : "  + req.hostname + ":" + PORT + req.url);
+
 // &&&&&&&&&&&&&&&&&&&&&&&&
 
 /*
@@ -26,13 +28,25 @@ WR__ 01 else res.redirect('http://' + req.hostname + req.url: 127.0.0.1 : /
 	// NOT WORKING! ! !  if (req.headers['x-forwarded-proto'] === 'http') {
 // &&&&&&&&&&&&&&&&&&&&&&&&
   if (req.protocol === 'http') {
-		console.log("WR__ 01 HTTP IF req.hostname + req.url + req.protocol + req.secure : "  + req.hostname + " : " + req.url + " : " + req.protocol + " : " + req.secure);
+		console.log("WR__ 01 HTTP IF. NEW RE-ROUTE req.hostname + req.url + req.protocol + req.secure : "  + req.hostname + " : " + req.url + " : " + req.protocol + " : " + req.secure);
+		// SEEMINGLY VERY REDUNDANT!
+		// BUT EVEN HTTPS:// IS COMING INTO THIS 'IF'. ?? HMM.
+		// LET'S FORCE THOSE HTTPS:// TO GO TO HTTP://
+		// WHILE THE HTTP:// GET RE-ROUTED (OH WELL) TO HTTP://
+// //			res.redirect('http://' + req.hostname + req.url);
+// //    res.redirect('http://' + req.hostname + ":" + PORT + req.url);
+
+// OOPS. Guess they thought of that: ERR_TOO_MANY_REDIRECTS !
+// But at least I'm landing on PORT 3000 now.
+// Sheesh.
+
 		next();
 	} else  // HTTPS = Not OK. Make it http
 	{
-		console.log("WR__ 02 HTTP S ELSE res.redirect('http://' + req.hostname + req.url: "  + req.hostname + " : " + req.url);
+		console.log("WR__ 02 HTTP S ELSE res.redirect('http://' + req.hostname + req.url: "  + req.hostname + ":" + PORT + req.url);
 
-		res.redirect('http://' + req.hostname + req.url);
+// console.log("WR__ 00A PORT STUFF req.hostname + :PORT + req.url : "  + req.hostname + ":" + PORT + req.url);
+		res.redirect('http://' + req.hostname + ":" + PORT + req.url);
 	}
 });
 // &&&&&&&&&&&&&&&&&&&&&&&&
