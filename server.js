318413bfc74320971408a99afb6db82f94d91f76
William Reilly <wreilly2001@gmail.com>
2016-08-07 11:22:45 -0400

    Additional logging for HTTP vs. HTTPS in server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const PORT = process.env.PORT || 3000;
 // Heroku will pass PORT in ...
 
 // Express middleware -- with every Request:
+// &&&&&&&&&&&&&&&&&&&&&&&&
 app.use(function (req, res, next) {
 			console.log("WR__ 00 req.hostname + req.url + req.protocol + req.secure : "  + req.hostname + " : " + req.url + " : " + req.protocol + " : " + req.secure);
+// &&&&&&&&&&&&&&&&&&&&&&&&
 
 /*
 wreillymc-l:ReactWeather william.reilly$ node server.js
@@ -22,15 +24,18 @@ WR__ 01 else res.redirect('http://' + req.hostname + req.url: 127.0.0.1 : /
 
 	// HTTP = OK
 	// NOT WORKING! ! !  if (req.headers['x-forwarded-proto'] === 'http') {
+// &&&&&&&&&&&&&&&&&&&&&&&&
   if (req.protocol === 'http') {
+		console.log("WR__ 01 HTTP IF req.hostname + req.url + req.protocol + req.secure : "  + req.hostname + " : " + req.url + " : " + req.protocol + " : " + req.secure);
 		next();
 	} else  // HTTPS = Not OK. Make it http
 	{
-		console.log("WR__ 01 else res.redirect('http://' + req.hostname + req.url: "  + req.hostname + " : " + req.url);
+		console.log("WR__ 02 HTTP S ELSE res.redirect('http://' + req.hostname + req.url: "  + req.hostname + " : " + req.url);
 
 		res.redirect('http://' + req.hostname + req.url);
 	}
 });
+// &&&&&&&&&&&&&&&&&&&&&&&&
 
 app.use(express.static('public'));
 
