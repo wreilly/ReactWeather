716b0e79bfee0d82a8ecede91d3248e1aea24980
William Reilly <wreilly2001@gmail.com>
2016-08-08 06:51:34 -0400

    Foundation top-bar, menu, in Nav.jsx (left side)

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,31 +2,29 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 
+// CSS note: JSX uses attribute 'class' for various purposes
+// So for HTML CSS 'class' we must use 'className'
 var Nav = () => {
   return (
-    <div>
-      <h3>Navigation Here 1</h3>
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Get Some Weather</IndexLink> |
-    <Link to="/about" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Get Some About</Link> |
-    <Link to="/examples" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Rots of Examples</Link>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Weather App</li>
+          <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>About</Link>
+          </li>
+          <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Examples</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+      </div>
     </div>
-    );
+  )
 };
 
 module.exports = Nav;
-
-// OLD WAY:
-// var Nav = React.createClass({
-//   render: function () {
-//   return (
-//     <div>
-//       <h3>Navigation Here 1</h3>
-// {/* Nope:          <p>heck go to / (home zat is)<a href="/">click</a></p>     */}
-//     <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Get Some Weather</IndexLink> |
-//     <Link to="/about" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Get Some About</Link> |
-//     <Link to="/examples" activeClassName="active" activeStyle={{fontWeight : 'bold'}}>Rots of Examples</Link>
-//     </div>
-//     );
-//   }
-// });
-//
