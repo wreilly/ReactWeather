0dbc4bcaf204a529e6b86ead64be4be3c42fd314
William Reilly <wreilly2001@gmail.com>
2016-08-19 10:09:23 -0400

    Fixed Error Modal viz. Foundation modifies DOM, goofs up React. We now use ReactDOMServer

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   getDefaultProps : function () {
@@ -13,15 +15,10 @@ var ErrorModal = React.createClass({
   },
   // https://facebook.github.io/react/docs/component-specs.html#lifecycle-methods
   componentDidMount : function () {
-     var modal = new Foundation.Reveal($('#error-modal')); // could also use refs ... hmm
-     modal.open();
-   },
-   // hmm data-reveal vs. data-reveal="" ??
-   // hmm data-close vs. data-close="" ??
-  //  Only with the  =""  did the OK button actually close it. hmm.
-  render : function () {
     var {title, message} = this.props;
-    return (
+
+    var modalMarkup = (
+      // all this WAS in render()
       <div className="reveal tiny text-center" id="error-modal" data-reveal="">
         <h4>{title}</h4>
         <p>Our error message for you.</p>
@@ -31,6 +28,31 @@ var ErrorModal = React.createClass({
             O.K.
           </button>
         </p>
+      </div>
+    );
+
+// jQuery selector
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+// returns DOM Node where "this" component is ...
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+     var modal = new Foundation.Reveal($('#error-modal')); // could also use refs ... hmm
+     modal.open();
+   },
+   // hmm data-reveal vs. data-reveal="" ??
+   // hmm data-close vs. data-close="" ??
+  //  Only with the  =""  did the OK button actually close it. hmm.
+  render : function () {
+    return (
+      <div>
+        // empty. min requirement.
+        // we've moved it all up to componentDidMount
+        // why? Foundation is Modifying the DOM, and that goofs up React.
+        // so, as I understand it, we use ReactDOMServer to create the component's markup on the server
+        // Sounds like, once rendered, by React, in that fashion (done on the server), React
+        // no longer expects the resultant DOM to be minded by React (on the client)
+        // So, if it changes (by Foundation), React (on the client) doesn't expect anything, and nothing breaks. Hmm.
+        // I do not claim to understand this, so very much ...
       </div>
     );
   }
